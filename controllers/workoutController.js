6c61e0b7826fb9f06a7c9f667173396f08940ae0
FDR <delrincon@gmail.com>
2022-06-22 12:10:50 -0300

    controllers and models done, backend OK

---
+++ controllers/workoutController.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+const Workout = require('../models/workoutModel');
+
+//1)  get all workouts
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1 });
+    
+    res.status(200).json( workouts );
+}
+
+//2)  get a single workout
+const getWorkout = async (req, res) => {
+    const {id} = req.params
+    
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No valid id'})
+    }
+
+    const workout = await Workout.findById(id);
+
+    if( !workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    
+    return res.status(200).json(workout);
+}
+
+
+//3)  create a new workout
+const createWorkout = async (req, res) => {
+    const { title, load, reps } = req.body;
+
+    // add doc to db
+    try {
+        const workout = await Workout.create({ title, reps, load })
+        res.status(200).json(workout)
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ error: error.message })
+    }
+}
+
+//4)   delete a workout
+const deleteWorkout = async (req, res) => {
+    const {id} = req.params
+    
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No valid id'})
+    }
+
+    const workout = await Workout.findOneAndDelete({ _id: id });//q el _id de mongo sea igual al id q yo le paso
+
+    if( !workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    
+    return res.status(200).json(workout);
+}
+
+//5)  update a workout
+const updateWorkout = async (req, res) => {
+    const {id} = req.params
+    // const { title, load, reps } = req.body;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No valid id'})
+    }
+
+    // const workout = await Workout.findOneAndUpdate({ _id: id }, { title, reps, load });
+    const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+    if( !workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    
+    return res.status(200).json(workout);
+}
+
+
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout,
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const workoutRoutes = require('./routes/workouts');
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 
 // express app
@@ -11,17 +12,27 @@ const app = express();
 app.use( express.json() );
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
+    // console.log(req.path, req.method);
     next();
 });
 
 // Routes
 app.use('/api/workouts', workoutRoutes );
 
-// listen for request
-app.listen( process.env.PORT, () => {
-    console.log('Listening on port 4000!!! ')
-});
+//  connect to db
+mongoose.connect( process.env.MONGO_URI )
+    .then(() => {
+        // listen for request
+        console.log('connect to DB - OK');
+        app.listen( process.env.PORT, () => {
+            console.log('Listening on port 4000!!! ')
+        });
+    })
+    .catch((error)=>{
+        console.log('connect to DB - ERROR');
+        console.log(error)
+    })
+
 
 process.env.PORT
 
